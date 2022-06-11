6b397479df978430181906994860f82258501620
DarkAce65 <tvdood99@gmail.com>
2022-06-11 19:12:15 +0530

    Bootstrap Vite app

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+const prettierConfig = require('./prettier.config');
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', 'import'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': ['error', 'nofunc'],
+    '@typescript-eslint/no-var-requires': 'off',
+    'prettier/prettier': ['warn', prettierConfig],
+    eqeqeq: 'warn',
+    'guard-for-in': 'error',
+    'no-console': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'no-multiple-empty-lines': ['warn', { max: 1 }],
+    'no-duplicate-imports': 'warn',
+    'no-shadow': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'warn',
+    'one-var': ['warn', 'never'],
+    'prefer-const': 'warn',
+    'prefer-template': 'warn',
+    'sort-imports': ['warn', { ignoreDeclarationSort: true }],
+    'spaced-comment': ['warn', 'always', { markers: ['/'] }],
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', ['internal', 'parent'], 'sibling', 'index'],
+        pathGroups: [
+          { pattern: 'vite', group: 'external', position: 'before' },
+          { pattern: 'vitest', group: 'external', position: 'before' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: false },
+      },
+    ],
+  },
+};
