6be20856ed43c70d046bf2e0025b9c72264d8d26
DarkAce65 <tvdood99@gmail.com>
2022-06-11 20:25:19 +0530

    Set up test utility for logic gates

---
+++ test/runLogicTests.ts
@@ -0,0 +1,45 @@
+import { expect, test } from 'vitest';
+
+import { Bool } from '../src/main';
+
+const toBinaryString = (num: number, length: number): string =>
+  (new Array(length).fill('0').join('') + num.toString(2)).slice(-length);
+
+function runLogicTests<T extends Bool[]>(
+  logicGateName: string,
+  gateFunction: (...args: T) => Bool,
+  truthTable: [T, Bool][]
+) {
+  if (truthTable.length === 0) {
+    throw new Error('Truth table cannot be empty!');
+  }
+
+  const numInputs = truthTable[0][0].length;
+
+  test(`META: ${logicGateName} coverage`, () => {
+    const cases = new Set();
+    for (const [inputs] of truthTable) {
+      cases.add(inputs.join(''));
+    }
+
+    const missingCases = [];
+    for (let i = 0; i < Math.pow(2, numInputs); i++) {
+      const inputs = toBinaryString(i, numInputs);
+      if (!cases.has(inputs)) {
+        missingCases.push(inputs);
+      }
+    }
+
+    expect(missingCases).toHaveLength(0);
+  });
+
+  for (const [inputs, expected] of truthTable.sort(([a], [b]) =>
+    a.join('').localeCompare(b.join(''))
+  )) {
+    test(`${logicGateName}(${inputs.join(', ')}) === ${expected}`, () => {
+      expect(gateFunction(...inputs)).toBe(expected);
+    });
+  }
+}
+
+export default runLogicTests;
