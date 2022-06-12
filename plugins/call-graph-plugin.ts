e808d390c43bdda7e6ce5c439265c7bb8de0a691
DarkAce65 <tvdood99@gmail.com>
2022-06-12 16:06:28 -0400

    Start building call-graph-plugin to track gate calls

---
+++ plugins/call-graph-plugin.ts
@@ -0,0 +1,55 @@
+import type { Plugin } from 'vite';
+
+import {
+  CallExpression,
+  Node,
+  ScriptTarget,
+  SourceFile,
+  createSourceFile,
+  isCallExpression,
+  isFunctionDeclaration,
+} from 'typescript';
+
+const findCallExpressions = (node: Node, source: SourceFile): CallExpression[] => {
+  const expressions = [];
+
+  if (isCallExpression(node)) {
+    expressions.push(node);
+  }
+  for (const child of node.getChildren(source)) {
+    expressions.push(...findCallExpressions(child, source));
+  }
+
+  return expressions;
+};
+
+function callGraphPlugin(): Plugin {
+  const callCounts = {};
+
+  return {
+    name: 'call-graph',
+    transform(code, id) {
+      if (id.includes('node_modules')) {
+        return;
+      }
+
+      const source = createSourceFile(id, code, ScriptTarget.ESNext);
+      source.forEachChild((sourceNode) => {
+        if (isFunctionDeclaration(sourceNode)) {
+          callCounts[sourceNode.name.text] = {};
+          for (const callExpression of findCallExpressions(sourceNode, source)) {
+            const functionName = callExpression.expression.getText(source);
+            if (!callCounts[sourceNode.name.text][functionName]) {
+              callCounts[sourceNode.name.text][functionName] = 0;
+            }
+            callCounts[sourceNode.name.text][functionName] += 1;
+          }
+        }
+      });
+
+      console.log(callCounts);
+    },
+  };
+}
+
+export default callGraphPlugin;
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import * as path from 'path';
 
 import { defineConfig } from 'vite';
 
+import callGraphPlugin from './plugins/call-graph-plugin';
+
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src'),
     },
   },
+  plugins: [callGraphPlugin()],
 });
