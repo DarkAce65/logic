af0a1c796212353811f58230db5b4ce2becec70a
DarkAce65 <tvdood99@gmail.com>
2022-06-17 23:01:06 -0400

    Print all gate counts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
+import { and } from './basic/and';
+import { nand } from './basic/nand';
+import { not } from './basic/not';
+import { or } from './basic/or';
+import { xor } from './basic/xor';
+
 export type Bool = 0 | 1;
 export interface WithGateCounts {
   gateCounts: { [gate: string]: number };
 }
+
+const ALL_GATES: { [gate: string]: WithGateCounts } = { and, nand, not, or, xor };
+
+console.log(
+  Object.keys(ALL_GATES).reduce((acc, gate) => ({ ...acc, [gate]: ALL_GATES[gate].gateCounts }), {})
+);
