0850f168c1b29f586aef17de903bf194952802f2
DarkAce65 <tvdood99@gmail.com>
2022-06-19 12:12:25 -0400

    Build and render Sankey graphs for each gate

--- src/main.ts
+++ src/main.ts
@@ -25,29 +25,28 @@ const ALL_GATES: { [gate: string]: WithGateCounts } = { and, nand, not, or, xor 
 interface GateGraphData {
   gate: string;
   totalNANDGates: number;
-  childGateGraph?: GateGraphData[];
+  childGateGraphs?: GateGraphData[];
 }
 const buildAllGateGraphData = (): { [gate: string]: GateGraphData } => {
   const allGateGraphData: { [gate: string]: GateGraphData } = {};
 
-  const buildGateGraphData = (gate: string): GateGraphData => {
-    if (Object.prototype.hasOwnProperty.call(allGateGraphData, gate)) {
-      return JSON.parse(JSON.stringify(allGateGraphData[gate]));
-    } else if (!Object.prototype.hasOwnProperty.call(ALL_GATES, gate)) {
+  const buildGateGraphData = (gate: string, gateCount = 1): GateGraphData => {
+    if (!Object.prototype.hasOwnProperty.call(ALL_GATES, gate)) {
       return { gate, totalNANDGates: 0 };
     } else if (gate === 'nand') {
-      return { gate, totalNANDGates: 1 };
+      return { gate, totalNANDGates: gateCount };
     }
 
     let totalNANDGates = 0;
-    const childGateGraph: GateGraphData[] = [];
+    const childGateGraphs: GateGraphData[] = [];
     for (const childGate of Object.keys(ALL_GATES[gate].gateCounts)) {
-      const gateGraphData = buildGateGraphData(childGate);
-      totalNANDGates += gateGraphData.totalNANDGates;
-      childGateGraph.push(gateGraphData);
+      const childGateCount = gateCount * ALL_GATES[gate].gateCounts[childGate];
+      const childGateGraphData = buildGateGraphData(childGate, childGateCount);
+      totalNANDGates += childGateGraphData.totalNANDGates;
+      childGateGraphs.push(childGateGraphData);
     }
 
-    return { gate, totalNANDGates, childGateGraph };
+    return { gate, totalNANDGates, childGateGraphs };
   };
 
   for (const gate of Object.keys(ALL_GATES)) {
@@ -57,7 +56,10 @@ const buildAllGateGraphData = (): { [gate: string]: GateGraphData } => {
   return allGateGraphData;
 };
 
-interface Node {}
+interface Node {
+  gate: string;
+  totalNANDGates: number;
+}
 interface Link {}
 interface SankeyLayouts {
   [gate: string]: (width: number, height: number) => SankeyGraph<Node, Link>;
@@ -66,86 +68,101 @@ const buildSankeyLayouts = (allGateGraphData: { [gate: string]: GateGraphData })
   const sankeyLayouts: SankeyLayouts = {};
 
   for (const gate of Object.keys(allGateGraphData)) {
+    const nodesById: { [gate: string]: SankeyNodeMinimal<Node, Link> & Node } = {};
     const nodes: (SankeyNodeMinimal<Node, Link> & Node)[] = [];
     const links: (SankeyLinkMinimal<Node, Link> & Link)[] = [];
-    nodes.push({});
-    nodes.push({});
-    nodes.push({});
-    links.push({ source: 1, target: 0, value: 1 });
-    links.push({ source: 1, target: 2, value: 2 });
-    links.push({ source: 0, target: 2, value: 4 });
+
+    const traverseGraph = (graph: GateGraphData) => {
+      if (Object.prototype.hasOwnProperty.call(nodesById, graph.gate)) {
+        nodesById[graph.gate].totalNANDGates += graph.totalNANDGates;
+      } else {
+        const node = { gate: graph.gate, totalNANDGates: graph.totalNANDGates };
+        nodes.push(node);
+        nodesById[graph.gate] = node;
+      }
+      if (graph.childGateGraphs) {
+        for (const childGraph of graph.childGateGraphs) {
+          links.push({
+            source: graph.gate,
+            target: childGraph.gate,
+            value: childGraph.totalNANDGates,
+          });
+          traverseGraph(childGraph);
+        }
+      }
+    };
+    traverseGraph(allGateGraphData[gate]);
+
     sankeyLayouts[gate] = (width, height) =>
-      sankey<Node, Link>().extent([
-        [50, 50],
-        [width - 50, height - 50],
-      ])({ nodes, links });
+      sankey<Node, Link>()
+        .nodeId((node) => node.gate)
+        .extent([
+          [50, 50],
+          [width - 50, height - 50],
+        ])({ nodes, links });
   }
 
   return sankeyLayouts;
 };
 
 const sankeyLayouts = buildSankeyLayouts(buildAllGateGraphData());
-console.log(sankeyLayouts);
 
-const renderGateVisualizations = (element: HTMLElement) => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+const renderGateVisualizations = (element: HTMLElement): ((gate: string) => void) => {
+  let width = element.clientWidth;
+  let height = element.clientHeight;
 
-  const sankeyLayout = sankeyLayouts['xor'](width, height);
+  const svg = d3Create('svg').style('font', '10px sans-serif');
 
-  const svg = d3Create('svg')
-    .attr('viewBox', [0, 0, width, height])
-    .style('font', '10px sans-serif')
-    .style('width', `${width}px`)
-    .style('height', `${height}px`);
-
-  svg
-    .append('g')
-    .selectAll('rect')
-    .data(sankeyLayout.nodes)
-    .join('rect')
-    .attr('x', (d) => d.x0!)
-    .attr('y', (d) => d.y0!)
-    .attr('height', (d) => d.y1! - d.y0!)
-    .attr('width', (d) => d.x1! - d.x0!);
-
-  const link = svg
-    .append('g')
-    // .attr("fill", "none")
-    // .attr("stroke-opacity", linkStrokeOpacity)
-    .selectAll('g')
-    .data(sankeyLayout.links)
-    .join('g')
-    .attr('fill', 'none')
-    .attr('stroke', '#000')
-    .attr('stroke-opacity', 0.2);
-  // .style("mix-blend-mode", linkMixBlendMode);
-
-  link
-    .append('path')
-    .attr('d', sankeyLinkHorizontal())
-    .attr('stroke-width', ({ width: w }) => Math.max(1, w!));
-  // .attr("stroke", linkColor === "source-target" ? ({index: i}) => `url(#${uid}-link-${i})`
-  //     : linkColor === "source" ? ({source: {index: i}}) => color(G[i])
-  //     : linkColor === "target" ? ({target: {index: i}}) => color(G[i])
-  //     : linkColor)
-  // .attr("stroke-width", ({width}) => Math.max(1, width))
-  // .call(Lt ? path => path.append("title").text(({index: i}) => Lt[i]) : () => {});
-
-  svg
-    .append('g')
-    // .attr("font-family", "sans-serif")
-    .attr('font-size', 16)
-    .selectAll('text')
-    .data(sankeyLayout.nodes)
-    .join('text')
-    .attr('x', (d) => (d.x0! < width / 2 ? d.x1! + 10 : d.x0! - 10))
-    .attr('y', (d) => (d.y1! + d.y0!) / 2)
-    .attr('dy', '0.35em')
-    .attr('text-anchor', (d) => (d.x0! < width / 2 ? 'start' : 'end'))
-    .text(() => 'test');
+  const nodes = svg.append('g');
+  const links = svg.append('g');
+  const text = svg.append('g');
 
   element.appendChild(svg.node()!);
+
+  return (gate: string) => {
+    width = element.clientWidth;
+    height = element.clientHeight;
+
+    const sankeyLayout = sankeyLayouts[gate](width, height);
+
+    svg
+      .attr('viewBox', [0, 0, width, height])
+      .style('width', `${width}px`)
+      .style('height', `${height}px`);
+
+    nodes
+      .selectAll('rect')
+      .data(sankeyLayout.nodes)
+      .join('rect')
+      .attr('x', (d) => d.x0!)
+      .attr('y', (d) => d.y0!)
+      .attr('height', (d) => d.y1! - d.y0!)
+      .attr('width', (d) => d.x1! - d.x0!);
+
+    links
+      .selectAll('path')
+      .data(sankeyLayout.links)
+      .join('path')
+      .attr('fill', 'none')
+      .attr('stroke', '#000')
+      .attr('stroke-opacity', 0.2)
+      .attr('d', sankeyLinkHorizontal())
+      .attr('stroke-width', ({ width: w }) => Math.max(1, w!))
+      .append('title')
+      .text((d) => d.value);
+
+    text
+      .attr('font-size', 16)
+      .selectAll('text')
+      .data(sankeyLayout.nodes)
+      .join('text')
+      .attr('x', (d) => (d.x0! < width / 2 ? d.x1! + 10 : d.x0! - 10))
+      .attr('y', (d) => (d.y1! + d.y0!) / 2)
+      .attr('dy', '0.35em')
+      .attr('text-anchor', (d) => (d.x0! < width / 2 ? 'start' : 'end'))
+      .text((d) => `${d.gate} ${d.totalNANDGates}`);
+  };
 };
 
-renderGateVisualizations(document.body);
+const render = renderGateVisualizations(document.querySelector('#container')!);
+render('xor');
