461875ba0d2b5e48d1070bcc05c1232da5a9c095
DarkAce65 <tvdood99@gmail.com>
2022-06-20 11:06:06 -0400

    Add checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import * as path from 'path';
 
 import { defineConfig } from 'vite';
 
+import checker from 'vite-plugin-checker';
+
 import callCountPlugin from './plugins/call-count-plugin';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src'),
     },
   },
-  plugins: [callCountPlugin()],
-});
+  plugins: [
+    callCountPlugin(),
+    checker({
+      overlay: { initialIsOpen: false },
+      terminal: mode !== 'test',
+      typescript: true,
+      eslint: { lintCommand: "eslint './src/**/*.{ts,tsx}'" },
+    }),
+  ],
+}));
