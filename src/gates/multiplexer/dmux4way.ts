123c2464e68f758561adc7bb15e984996fd5938c
DarkAce65 <tvdood99@gmail.com>
2023-06-19 15:48:24 -0400

    Optimize multiway dmux gates using core dmux

--- src/gates/multiplexer/dmux4way.ts
+++ src/gates/multiplexer/dmux4way.ts
@@ -1,14 +1,13 @@
 import { Bool, Tuple } from '@/types';
 
-import { and } from '../basic/and';
-import { not } from '../basic/not';
+import { dmux } from '../basic/dmux';
 
 export function dmux4way(input: Bool, sel: Tuple<Bool, 2>): Tuple<Bool, 4> {
-  return [
-    and(input, and(not(sel[0]), not(sel[1]))),
-    and(input, and(not(sel[0]), sel[1])),
-    and(input, and(sel[0], not(sel[1]))),
-    and(input, and(sel[0], sel[1])),
-  ];
+  const [pow1High, pow1Low] = dmux(input, sel[0]);
+
+  const [pow1High_pow0High, pow1High_pow0Low] = dmux(pow1High, sel[1]);
+  const [pow1Low_pow0High, pow1Low_pow0Low] = dmux(pow1Low, sel[1]);
+
+  return [pow1High_pow0High, pow1High_pow0Low, pow1Low_pow0High, pow1Low_pow0Low];
 }
 dmux4way.gateCounts = {};
--- src/gates/multiplexer/dmux8way.ts
+++ src/gates/multiplexer/dmux8way.ts
@@ -1,18 +1,27 @@
 import { Bool, Tuple } from '@/types';
 
-import { and } from '../basic/and';
-import { not } from '../basic/not';
+import { dmux } from '../basic/dmux';
 
 export function dmux8way(input: Bool, sel: Tuple<Bool, 3>): Tuple<Bool, 8> {
+  const [pow2High, pow2Low] = dmux(input, sel[0]);
+
+  const [pow2High_pow1High, pow2High_pow1Low] = dmux(pow2High, sel[1]);
+  const [pow2Low_pow1High, pow2Low_pow1Low] = dmux(pow2Low, sel[1]);
+
+  const [pow2High_pow1High_pow0High, pow2High_pow1High_pow0Low] = dmux(pow2High_pow1High, sel[2]);
+  const [pow2High_pow1Low_pow0High, pow2High_pow1Low_pow0Low] = dmux(pow2High_pow1Low, sel[2]);
+  const [pow2Low_pow1High_pow0High, pow2Low_pow1High_pow0Low] = dmux(pow2Low_pow1High, sel[2]);
+  const [pow2Low_pow1Low_pow0High, pow2Low_pow1Low_pow0Low] = dmux(pow2Low_pow1Low, sel[2]);
+
   return [
-    and(input, and(not(sel[0]), and(not(sel[1]), not(sel[2])))),
-    and(input, and(not(sel[0]), and(not(sel[1]), sel[2]))),
-    and(input, and(not(sel[0]), and(sel[1], not(sel[2])))),
-    and(input, and(not(sel[0]), and(sel[1], sel[2]))),
-    and(input, and(sel[0], and(not(sel[1]), not(sel[2])))),
-    and(input, and(sel[0], and(not(sel[1]), sel[2]))),
-    and(input, and(sel[0], and(sel[1], not(sel[2])))),
-    and(input, and(sel[0], and(sel[1], sel[2]))),
+    pow2High_pow1High_pow0High,
+    pow2High_pow1High_pow0Low,
+    pow2High_pow1Low_pow0High,
+    pow2High_pow1Low_pow0Low,
+    pow2Low_pow1High_pow0High,
+    pow2Low_pow1High_pow0Low,
+    pow2Low_pow1Low_pow0High,
+    pow2Low_pow1Low_pow0Low,
   ];
 }
 dmux8way.gateCounts = {};
